dd3ca05a4f2c9c39d19ee1ecf59e652c821de10f
Juan Rossa <juanmrossa2002@gmail.com>
2022-07-30 18:38:06 -0300

    commit on 'main'

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -8,34 +8,49 @@ import { Usuarios } from './usuarios';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
-
-  Valorswitch = "asd"
-  nombre = "asdasd" ;
-  apellido = "aasda";
-  Genero = "asdasda";
-
-
-  Usuarios: Usuarios=new Usuarios(this.nombre, this.apellido, this.Genero);
+ 
 
 
 
 
 
-  verDatos(){
-    console.log('Nombre: ', this.nombre) ;
-    console.log('Apellido: ', this.apellido) ;
-    console.log('Genero: ', this.Genero) ;
-
-  }
-
-
-  imprimirDatos(){
-    if(this.Genero == "masculino" || this.Genero == "femenino"){
-      this.Valorswitch = this.Genero;
-    }
-  }
 
   constructor() { }
+  nombre = ""
+  apellido = ""
+  Genero = ""
+  click = 0 
+  contador = 0
+  alumnostotal:any[] = []
+
+
+
+  CrearAlumno(){
+    let alumnos = new Usuarios (this.nombre, this.apellido, this.Genero);
+    for(let i=0; this.click<1; i++){
+      this.alumnostotal[this.contador] = alumnos;
+      console.log(this.alumnostotal[this.contador]);
+      this.click =1
+      this.contador ++ 
+      console.log(this.contador)
+    }
+    this.click=0;
+   
+      
+    
+  }
+
+  MostrarAlumnos(){
+    for(let i = 0; this.contador>i ; i++){
+        console.log(this.alumnostotal[i])
+    }
+
+  }
+
+
+
+
+
 
   ngOnInit(): void {
   }
